cbcb5cd6243633cb0c4b0ea897cfeb3eea4b6bac
pragmatic335 <pragmatik335@gmail.com>
2021-03-31 16:31:57 +0700

    '0_)'

--- views/layouts/generaly.php
+++ views/layouts/generaly.php
@@ -24,19 +24,23 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
-<div class="wrap">
 
-<?php $this->beginBody() ?>
+<div id="massager">
+
+    <div id="context" class="container-xl">
+        <?php $this->beginBody() ?>
         <?= $content ?>
+    </div>
+
 </div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+<!--<footer class="footer">-->
+<!--    <div class="container">-->
+<!--        <p class="pull-left">&copy; My Company --><?//= date('Y') ?><!--</p>-->
+<!---->
+<!--        <p class="pull-right">--><?//= Yii::powered() ?><!--</p>-->
+<!--    </div>-->
+<!--</footer>-->
 
 <?php $this->endBody() ?>
 </body>
--- views/site/test.php
+++ views/site/test.php
@@ -3,227 +3,25 @@
 ?>
 
 <div class="row">
-    <div class = "col-lg-10 col-md-9 col-sm-8 col" style="background-color: #1c7430">
-        <p>Hello messages</p>
-    </div>
-    <div class="col-lg-2 col-md-3 col-sm-4 col">
+    <div class="col-lg-2 col-md-3 col-sm-3 col-4">
         <div class="d-flex flex-column">
-            <div class="container">
-                <div class="overflow-auto" style="height: calc(100vh - 120px);">
+                <div class="overflow-auto" style="height: inherit">
                     <div class="user shadow-sm">
                         <div class="row" style="width: 150px; height: inherit">
                             <div class="col-3 align-self-center">
                                 <div class="indicator"></div>
                             </div>
                             <div class="col-9 align-self-center">
-                                <h4 class="usernick">Марина</h4>
+                                <h6 class="usernick">Марина</h6>
                             </div>
                         </div>
                     </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="user">
-                        <div class="row" style="width: 150px; height: inherit">
-                            <div class="col-3 align-self-center">
-                                <div class="indicator"></div>
-                            </div>
-                            <div class="col-9 align-self-center">
-                                <h3 class="usernick">Марина</h3>
-                            </div>
-                        </div>
-                    </div>
-
-
 
                 </div>
-            </div>
         </div>
+    </div>
+    <div class = "col-lg-10 col-md-9 col-sm-9 col-8" style="background-color: #1c7430">
+        <p>Hello messages</p>
     </div>
 </div>
 
