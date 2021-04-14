b1f33c3c96b4f9c4928720976238a4f556665bc4
pragmatic335 <pragmatik335@gmail.com>
2021-04-14 16:44:55 +0700

    v1.1

--- widgets/views/chat.php
+++ widgets/views/chat.php
@@ -19,23 +19,35 @@
 <!--</div>-->
 
 <div id='prog-massager'>
+
     <div id="prog-marker">
         <i class="fa fa-comments-o" style='font-size: 42px'></i>
     </div>
+
     <div id="prog-content">
+
         <div id="prog-tools-bar">
             <i class="fa fa-bell-o"></i>
             <i class="fa fa-sign-out"></i>
         </div>
+
         <div id="prog-user-window">
             <div id="prog-user-rooms">
-
+                <div class="prog-user">
+                    <i class="fa fa-circle" style='font-size: 15px; color: chartreuse;'></i>
+                    <h5 style="margin-top: 5px;">Общая комната</h5>
+                </div>
+                <div class="prog-user">
+                    <i class="fa fa-circle" style='font-size: 15px; color: orangered;'></i>
+                    <h5 style="margin-top: 5px;">Марина</h5>
+                </div>
             </div>
             <div id="prog-user-messages">
 
             </div>
         </div>
     </div>
+
 </div>
 
 
